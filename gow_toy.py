1a7f3b2fd425f492b8823d47f612a7f915ac1a3d
Nikos Kanakaris <nikos.kanakaris89@gmail.com>
2020-03-21 17:21:37 +0200

    Refactor code

--- gow_toy.py
+++ gow_toy.py
@@ -1,6 +1,6 @@
 import string
 from nltk.corpus import stopwords
-from library import clean_text_simple, terms_to_graph, core_dec
+from library import clean_text_simple, GraphOfWordsUtils
 
 stpwds = stopwords.words('english')
 punct = string.punctuation.replace('-', '')
@@ -14,15 +14,10 @@ lies in reducing it to a numerical system of a special kind.'''
 my_doc = my_doc.replace('\n', '')
 
 # pre-process document
-my_tokens = clean_text_simple(my_doc,my_stopwords=stpwds,punct=punct, remove_stopwords=True, stemming=True, pos_filtering=False)
+my_tokens = clean_text_simple(my_doc,my_stopwords=stpwds,punct=punct, remove_stopwords=False, stemming=True, pos_filtering=False)
                               
-g = terms_to_graph(my_tokens, w=3)
+gow = GraphOfWordsUtils()
+graph = gow.convert_terms_to_graph(my_tokens, w=4)
 
-# decompose g
-core_numbers = core_dec(g, True)
-# print(core_numbers)
-
-# retain main core as keywords
-max_c_n = max(core_numbers.values())
-keywords = [kwd for kwd, c_n in core_numbers.items() if c_n == max_c_n]
+keywords = gow.get_main_core_keywords(graph, weighted=True)
 print(keywords)
--- library.py
+++ library.py
@@ -47,134 +47,120 @@ def clean_text_simple(text, my_stopwords, punct, remove_stopwords=True, pos_filt
     return(tokens)
 
 
-def terms_to_graph(terms, w):
-    '''This function returns a directed, weighted igraph from a list of terms (the tokens from the pre-processed text) e.g., ['quick','brown','fox'].
-    Edges are weighted based on term co-occurrence within a sliding window of fixed size 'w'.
-    '''
-    
-    from_to = {}
-    
-    # create initial complete graph (first w terms)
-    terms_temp = terms[0:w]
-    indexes = list(itertools.combinations(range(w), r=2))
-    
-    new_edges = []
-    
-    for my_tuple in indexes:
-        new_edges.append(tuple([terms_temp[i] for i in my_tuple]))
-    
-    for new_edge in new_edges:
-        if new_edge in from_to:
-            from_to[new_edge] += 1
-        else:
-            from_to[new_edge] = 1
-
-    # then iterate over the remaining terms
-    for i in range(w, len(terms)):
-        considered_term = terms[i] # term to consider
-        terms_temp = terms[(i-w+1):(i+1)] # all terms within sliding window
+class GraphOfWordsUtils:
+    def convert_terms_to_graph(self, terms, w):
+        """This function returns a directed, weighted igraph from a list of terms (the tokens from the pre-processed text) e.g., ['quick','brown','fox'].
+        Edges are weighted based on term co-occurrence within a sliding window of fixed size 'w'.
+        """
+        from_to = {}
         
-        # edges to try
-        candidate_edges = []
-        for p in range(w-1):
-            candidate_edges.append((terms_temp[p],considered_term))
-    
-        for try_edge in candidate_edges:
-            
-            if try_edge[1] != try_edge[0]:
-            # if not self-edge
-            
-                # if edge has already been seen, update its weight
-                if try_edge in from_to:
-                    from_to[try_edge] += 1
-                                   
-                # if edge has never been seen, create it and assign it a unit weight     
-                else:
-                    from_to[try_edge] = 1
-    
-    # create empty graph
-    g = igraph.Graph(directed=False)
-    
-    from_to_undirected = {}
-    for t1 in from_to.keys():
-        t2 = (t1[1], t1[0])
-        if t1 in from_to_undirected:
-            from_to_undirected[t1] += from_to[t1]
-        elif t2 in from_to_undirected:
-            from_to_undirected[t2] += from_to[t1]
-        else:
-            from_to_undirected[t1] = from_to[t1]
-
-    # add vertices
-    g.add_vertices(sorted(set(terms)))
-    
-    # add edges, direction is preserved since the graph is directed
-    g.add_edges(from_to_undirected.keys())
-    
-    # set edge and vertex weights
-    g.es['weight'] = list(from_to_undirected.values()) # based on co-occurence within sliding window
-    g.vs['weight'] = g.strength(weights=list(from_to_undirected.values())) # weighted degree
-    
-    return(g)
-
-
-def core_dec(g,weighted):
-    '''(un)weighted k-core decomposition'''
-    # work on clone of g to preserve g 
-    gg = copy.deepcopy(g)
-    if not weighted:
-        gg.vs['weight'] = gg.strength() # overwrite the 'weight' vertex attribute with the unweighted degrees
-    # initialize dictionary that will contain the core numbers
-    cores_g = dict(zip(gg.vs['name'],[0]*len(gg.vs)))
-    
-    while len(gg.vs) > 0:
-        # find index of lowest degree vertex
-        min_degree = min(gg.vs['weight'])
-        index_top = gg.vs['weight'].index(min_degree)
-        name_top = gg.vs[index_top]['name']
-        # get names of its neighbors
-        neighbors = gg.vs[gg.neighbors(index_top)]['name']
-        # exclude self-edges
-        neighbors = [elt for elt in neighbors if elt!=name_top]
-        # set core number of lowest degree vertex as its degree
-        cores_g[name_top] = min_degree
-        # delete top vertex and its incident edges
-        gg.delete_vertices(index_top)
+        # create initial complete graph (first w terms)
+        terms_temp = terms[0:w]
+        indexes = list(itertools.combinations(range(w), r=2))
         
-        if neighbors:
-            if weighted: 
-                new_degrees = gg.strength(weights=gg.es['weight'])
+        new_edges = []
+        
+        for my_tuple in indexes:
+            new_edges.append(tuple([terms_temp[i] for i in my_tuple]))
+        
+        for new_edge in new_edges:
+            if new_edge in from_to:
+                from_to[new_edge] += 1
             else:
-                new_degrees = gg.strength()
-            # iterate over neighbors of top element
-            for neigh in neighbors:
-                index_n = gg.vs['name'].index(neigh)
-                gg.vs[index_n]['weight'] = max(min_degree,new_degrees[index_n])  
+                from_to[new_edge] = 1
+
+        # then iterate over the remaining terms
+        for i in range(w, len(terms)):
+            considered_term = terms[i] # term to consider
+            terms_temp = terms[(i-w+1):(i+1)] # all terms within sliding window
+            
+            # edges to try
+            candidate_edges = []
+            for p in range(w-1):
+                candidate_edges.append((terms_temp[p],considered_term))
         
-    return(cores_g)
+            for try_edge in candidate_edges:
+                
+                if try_edge[1] != try_edge[0]:
+                # if not self-edge
+                
+                    # if edge has already been seen, update its weight
+                    if try_edge in from_to:
+                        from_to[try_edge] += 1
+                                       
+                    # if edge has never been seen, create it and assign it a unit weight     
+                    else:
+                        from_to[try_edge] = 1
+        
+        # create empty graph
+        g = igraph.Graph(directed=False)
+        
+        from_to_undirected = {}
+        for t1 in from_to.keys():
+            t2 = (t1[1], t1[0])
+            if t1 in from_to_undirected:
+                from_to_undirected[t1] += from_to[t1]
+            elif t2 in from_to_undirected:
+                from_to_undirected[t2] += from_to[t1]
+            else:
+                from_to_undirected[t1] = from_to[t1]
 
+        # add vertices
+        g.add_vertices(sorted(set(terms)))
+        
+        # add edges, direction is preserved since the graph is directed
+        g.add_edges(from_to_undirected.keys())
+        
+        # set edge and vertex weights
+        g.es['weight'] = list(from_to_undirected.values()) # based on co-occurence within sliding window
+        g.vs['weight'] = g.strength(weights=list(from_to_undirected.values())) # weighted degree
+        
+        return(g)
 
-def accuracy_metrics(candidate, truth):
-    
-    # true positives ('hits') are both in candidate and in truth
-    tp = len(set(candidate).intersection(truth))
-    
-    # false positives ('false alarms') are in candidate but not in truth
-    fp = len([element for element in candidate if element not in truth])
-    
-    # false negatives ('misses') are in truth but not in candidate
-    fn = len([element for element in truth if element not in candidate])
-    
-    # precision
-    prec = round(float(tp)/(tp+fp),5)
-    
-    # recall
-    rec = round(float(tp)/(tp+fn),5)
-    
-    if prec+rec != 0:
-        # F1 score
-        f1 = round(2 * float(prec*rec)/(prec+rec),5)
-    else:
-        f1 = 0
-       
-    return (prec, rec, f1)
+    def decompose(self, g, weighted):
+        """(un)weighted k-core decomposition"""
+        # work on clone of g to preserve g 
+        gg = copy.deepcopy(g)
+        if not weighted:
+            gg.vs['weight'] = gg.strength() # overwrite the 'weight' vertex attribute with the unweighted degrees
+        # initialize dictionary that will contain the core numbers
+        cores_g = dict(zip(gg.vs['name'],[0]*len(gg.vs)))
+        
+        while len(gg.vs) > 0:
+            # find index of lowest degree vertex
+            min_degree = min(gg.vs['weight'])
+            index_top = gg.vs['weight'].index(min_degree)
+            name_top = gg.vs[index_top]['name']
+            # get names of its neighbors
+            neighbors = gg.vs[gg.neighbors(index_top)]['name']
+            # exclude self-edges
+            neighbors = [elt for elt in neighbors if elt!=name_top]
+            # set core number of lowest degree vertex as its degree
+            cores_g[name_top] = min_degree
+            # delete top vertex and its incident edges
+            gg.delete_vertices(index_top)
+            
+            if neighbors:
+                if weighted: 
+                    new_degrees = gg.strength(weights=gg.es['weight'])
+                else:
+                    new_degrees = gg.strength()
+                # iterate over neighbors of top element
+                for neigh in neighbors:
+                    index_n = gg.vs['name'].index(neigh)
+                    gg.vs[index_n]['weight'] = max(min_degree,new_degrees[index_n])  
+            
+        return cores_g
+
+    def get_k_core_keywords(self, graph, weighted, minimum_k=None):
+        core_numbers = self.decompose(graph, weighted)
+        if minimum_k is None:
+            threshold_k = max(core_numbers.values())
+        else:
+            threshold_k = minimum_k
+        # retain main core as keywords
+        keywords = [kwd for kwd, c_n in core_numbers.items() if c_n >= threshold_k]
+        return keywords
+
+    def get_main_core_keywords(self, graph, weighted):
+        return self.get_k_core_keywords(graph, weighted)
